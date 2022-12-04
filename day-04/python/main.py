ecc62390d939921847297a9c7f8863ee00b310eb
Rahul Ahuja <rah.ahuja@gmail.com>
2022-12-04 12:54:29 -0600

    Add Python Day 4

---
+++ day-04/python/main.py
@@ -0,0 +1,36 @@
+import pathlib
+from typing import Callable
+
+
+def filtered_range_count(ranges: list[list[set[int]]], condition: Callable) -> int:
+    return len(list(filter(condition, ranges)))
+
+
+def part_one(ranges: list[list[set[int]]]) -> int:
+    return filtered_range_count(
+        ranges, lambda pair: pair[0].issubset(pair[1]) or pair[1].issubset(pair[0])
+    )
+
+
+def part_two(ranges: list[list[set[int]]]) -> int:
+    return filtered_range_count(ranges, lambda pair: pair[0].intersection(pair[1]))
+
+
+with open(pathlib.Path(__file__).parent.parent / 'input.txt') as f:
+    ranges = [
+        [set(range(start, end + 1)) for start, end in interval]
+        for interval in [
+            [[int(bound) for bound in bounds.split('-')] for bounds in interval]
+            for interval in [line.split(',') for line in f.read().splitlines()]
+        ]
+    ]
+
+
+part_one_solution = part_one(ranges)
+part_two_solution = part_two(ranges)
+
+print('Part One:', part_one_solution)
+print('Part Two:', part_two_solution)
+
+assert part_one_solution == 532
+assert part_two_solution == 854
